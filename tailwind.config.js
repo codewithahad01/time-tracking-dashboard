fa1c641ce18e6ee250f7c9282b7b4353cdb9e9fc
abdulahadahmadi <ab.ahmadiofficial@gmail.com>
2022-06-25 11:13:43 +0430

    some changes in app

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,12 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    screens: {
+      sm: '480px',
+      md: '786px',
+      lg: '976px',
+      xl: '1440px',
+    },
   },
   plugins: [],
 }
