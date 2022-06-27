b492380975d3b92266d443bbba71f66fae2110e3
abdulahadahmadi <ab.ahmadiofficial@gmail.com>
2022-06-27 21:52:01 +0430

    subcomponents for day week and monthly tracking added

--- components/Dashboard.jsx
+++ components/Dashboard.jsx
@@ -4,16 +4,17 @@ import Image from 'next/image'
 import Link from 'next/link'
 
 function Dashboard() {
-    // let content = 0
+    let content = 0
     
-    // data.map((dat) => {
-    //     console.log(dat.timeframes.daily.previous)
-    // })
+    data.map((dat) => {
+        return dat.timeframes
+        console.log(dat)
+    })
     return (
         <div className='container mx-auto flex flex-col  my-12 px-6'>
             <div className=''>
                 {/* start of profile */}
-                    <div className='flex flex-col justify-center overflow-hidden text-white md:p44 '>
+                    <div className=' md:grid-cols-1 md:grid-rows-1 md:h-full justify-center overflow-hidden text-white md:p44 '>
                         <div className='py-12 px-8  flex flex-row justify-centertext-white bg-violet-800 rounded-lg shadow-xl
                         '>
                             <Image className='' src='/images/image-jeremy.png' alt='profile-image' width={50} height={50}/>
@@ -22,24 +23,24 @@ function Dashboard() {
                                 <h2 className='text-xl'>jeremy Robson</h2>
                             </div>
                         </div>
-
+        
                         <div className='px-4 bg-blue-900 rounded-b-lg text-sm '>
                             <ul className='flex justify-center'>
-                                <li className='px-3 py-7 text-white'>Daily</li>
-                                <li className='px-4 py-7 text-white'>Weekly</li>
-                                <li className='px-4 py-7 text-white'>Monthly</li>
+                                <li className='px-3 py-7 text-white'><Link href='#home' aria-disabled><a >Daily</a></Link></li>
+                                <li className='px-4 py-7 text-white'><Link href='#home'><a >Weekly</a></Link></li>
+                                <li className='px-4 py-7 text-white'><Link href='#home'><a >Monthly</a></Link></li>
                             </ul>
                         </div>
                     </div>
                 {/* end of profile */}
-
-                <div className='mt-10 shadow-xl relative'>
+            <div className='grid md:grid-cols-3 md:gap-6'>
+                <div className='mt-10 shadow-xl relative '>
                     <div className='bg-orange-400 rounded-t-xl'>
                         <Image className='animate-bounce absolute ' src='/images/icon-work.svg' alt='work image' width={80} height={80} />
                     </div>
                     <div className='bg-blue-900 p-4 rounded-t-xl'>
                         <div className='flex justify-between  '>
-                            <span className='text-stone-200 text-xl '> { data[Object.keys(data)[0]]} </span>
+                            <span className='text-stone-200 text-xl '> {data.map(record => {return record.title[0]})} </span>
                             <Image className='h-2' src='/images/icon-ellipsis.svg' alt='elipsis image' layout='fixed' width={35} height={10} />
                         </div>
                         <div className='flex  justify-between pt-6 pb-4 md:flex-col md:text-center '>
@@ -49,6 +50,87 @@ function Dashboard() {
                     </div>
                 </div>
 
+                <div className='mt-10 shadow-xl relative'>
+                    <div className='bg-sky-400 rounded-t-xl  '>
+                        <Image className='animate-bounce opacity-40  float-right' src='/images/icon-play.svg' alt='work image' width={80} height={80} />
+                    </div>
+                    <div className='bg-blue-900 p-4 rounded-t-xl'>
+                        <div className='flex justify-between  '>
+                            <span className='text-stone-200 text-xl '> {data.map(record => {return record.title[0]})} </span>
+                            <Image className='h-2' src='/images/icon-ellipsis.svg' alt='elipsis image' layout='fixed' width={35} height={10} />
+                        </div>
+                        <div className='flex  justify-between pt-6 pb-4 md:flex-col md:text-center '>
+                            <span className='text-white text-4xl md:text-6xl'>32 hrs</span>
+                            <span className='text-white opacity-60'>Last Week-36hrs</span>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div className='mt-10 shadow-xl relative'>
+                    <div className='bg-red-400 rounded-t-xl'>
+                        <Image className='animate-bounce opacity-40 ' src='/images/icon-study.svg' alt='work image' width={80} height={80} />
+                    </div>
+                    <div className='bg-blue-900 p-4 rounded-t-xl'>
+                        <div className='flex justify-between  '>
+                            <span className='text-stone-200 text-xl '> {data.map(record => {return record.title[0]})} </span>
+                            <Image className='h-2' src='/images/icon-ellipsis.svg' alt='elipsis image' layout='fixed' width={35} height={10} />
+                        </div>
+                        <div className='flex  justify-between pt-6 pb-4 md:flex-col md:text-center '>
+                            <span className='text-white text-4xl md:text-6xl'>32 hrs</span>
+                            <span className='text-white opacity-60'>Last Week-36hrs</span>
+                        </div>
+                    </div>
+                </div>
+
+                <div className='mt-10 shadow-xl '>
+                    <div className='bg-green-400 rounded-t-xl'>
+                        <Image className='animate-bounce opacity-40 ' src='/images/icon-exercise.svg' alt='work image' width={80} height={80} />
+                    </div>
+                    <div className='bg-blue-900 p-4 rounded-t-xl'>
+                        <div className='flex justify-between  '>
+                            <span className='text-stone-200 text-xl '> {data.map(record => {return record.title[0]})} </span>
+                            <Image className='h-2' src='/images/icon-ellipsis.svg' alt='elipsis image' layout='fixed' width={35} height={10} />
+                        </div>
+                        <div className='flex  justify-between pt-6 pb-4 md:flex-col md:text-center '>
+                            <span className='text-white text-4xl md:text-6xl'>32 hrs</span>
+                            <span className='text-white opacity-60'>Last Week-36hrs</span>
+                        </div>
+                    </div>
+                </div>
+
+                <div className='mt-10 shadow-xl'>
+                    <div className='bg-violet-600 rounded-t-xl'>
+                        <Image className='animate-bounce opacity-40' src='/images/icon-social.svg' alt='work image' width={80} height={80} />
+                    </div>
+                    <div className='bg-blue-900 p-4 rounded-t-xl'>
+                        <div className='flex justify-between  '>
+                            <span className='text-stone-200 text-xl '> {data.map(record => {return record.title[0]})} </span>
+                            <Image className='h-2' src='/images/icon-ellipsis.svg' alt='elipsis image' layout='fixed' width={35} height={10} />
+                        </div>
+                        <div className='flex  justify-between pt-6 pb-4 md:flex-col md:text-center '>
+                            <span className='text-white text-4xl md:text-6xl'>32 hrs</span>
+                            <span className='text-white opacity-60'>Last Week-36hrs</span>
+                        </div>
+                    </div>
+                </div>
+
+                <div className='mt-10 shadow-xl '>
+                    <div className='bg-yellow-300 rounded-t-xl'>
+                        <Image className='animate-bounce opacity-40 ' src='/images/icon-self-care.svg' alt='work image' width={80} height={80} />
+                    </div>
+                    <div className='bg-blue-900 p-4 rounded-t-xl'>
+                        <div className='flex justify-between  '>
+                            <span className='text-stone-200 text-xl '> {data.map(record => {return record.title[0]})} </span>
+                            <Image className='h-2' src='/images/icon-ellipsis.svg' alt='elipsis image' layout='fixed' width={35} height={10} />
+                        </div>
+                        <div className='flex  justify-between pt-6 pb-4 md:flex-col md:text-center '>
+                            <span className='text-white text-4xl md:text-6xl'>32 hrs</span>
+                            <span className='text-white opacity-60'>Last Week-36hrs</span>
+                        </div>
+                    </div>
+                </div>      
+                </div>
             </div>
         </div>
     )
