68e866b6cac3cf16c6daf6cebb7badce0a1bf3f2
abdulahadahmadi <ab.ahmadiofficial@gmail.com>
2022-06-27 22:24:07 +0430

    grid elvel element added

--- components/Dashboard.jsx
+++ components/Dashboard.jsx
@@ -7,14 +7,14 @@ function Dashboard() {
     let content = 0
     
     data.map((dat) => {
-        return dat.timeframes
-        console.log(dat)
+        content = dat.split(' ')
+        console.log(dat.title[0])
     })
     return (
         <div className='container mx-auto flex flex-col  my-12 px-6'>
             <div className=''>
                 {/* start of profile */}
-                    <div className=' md:grid-cols-1 md:grid-rows-1 md:h-full justify-center overflow-hidden text-white md:p44 '>
+                    <div className='  justify-center overflow-hidden text-white md:p44 '>
                         <div className='py-12 px-8  flex flex-row justify-centertext-white bg-violet-800 rounded-lg shadow-xl
                         '>
                             <Image className='' src='/images/image-jeremy.png' alt='profile-image' width={50} height={50}/>
@@ -33,6 +33,7 @@ function Dashboard() {
                         </div>
                     </div>
                 {/* end of profile */}
+
             <div className='grid md:grid-cols-3 md:gap-6'>
                 <div className='mt-10 shadow-xl relative '>
                     <div className='bg-orange-400 rounded-t-xl'>
@@ -44,7 +45,7 @@ function Dashboard() {
                             <Image className='h-2' src='/images/icon-ellipsis.svg' alt='elipsis image' layout='fixed' width={35} height={10} />
                         </div>
                         <div className='flex  justify-between pt-6 pb-4 md:flex-col md:text-center '>
-                            <span className='text-white text-4xl md:text-6xl'>32 hrs</span>
+                            <span className='text-white text-4xl md:text-6xl md:mb-4'>32 hrs</span>
                             <span className='text-white opacity-60'>Last Week-36hrs</span>
                         </div>
                     </div>
@@ -60,7 +61,7 @@ function Dashboard() {
                             <Image className='h-2' src='/images/icon-ellipsis.svg' alt='elipsis image' layout='fixed' width={35} height={10} />
                         </div>
                         <div className='flex  justify-between pt-6 pb-4 md:flex-col md:text-center '>
-                            <span className='text-white text-4xl md:text-6xl'>32 hrs</span>
+                            <span className='text-white text-4xl md:text-6xl md:mb-4'>32 hrs</span>
                             <span className='text-white opacity-60'>Last Week-36hrs</span>
                         </div>
                     </div>
@@ -77,7 +78,7 @@ function Dashboard() {
                             <Image className='h-2' src='/images/icon-ellipsis.svg' alt='elipsis image' layout='fixed' width={35} height={10} />
                         </div>
                         <div className='flex  justify-between pt-6 pb-4 md:flex-col md:text-center '>
-                            <span className='text-white text-4xl md:text-6xl'>32 hrs</span>
+                            <span className='text-white text-4xl md:text-6xl md:mb-4'>32 hrs</span>
                             <span className='text-white opacity-60'>Last Week-36hrs</span>
                         </div>
                     </div>
@@ -93,7 +94,7 @@ function Dashboard() {
                             <Image className='h-2' src='/images/icon-ellipsis.svg' alt='elipsis image' layout='fixed' width={35} height={10} />
                         </div>
                         <div className='flex  justify-between pt-6 pb-4 md:flex-col md:text-center '>
-                            <span className='text-white text-4xl md:text-6xl'>32 hrs</span>
+                            <span className='text-white text-4xl md:text-6xl md:mb-4'>32 hrs</span>
                             <span className='text-white opacity-60'>Last Week-36hrs</span>
                         </div>
                     </div>
@@ -109,7 +110,7 @@ function Dashboard() {
                             <Image className='h-2' src='/images/icon-ellipsis.svg' alt='elipsis image' layout='fixed' width={35} height={10} />
                         </div>
                         <div className='flex  justify-between pt-6 pb-4 md:flex-col md:text-center '>
-                            <span className='text-white text-4xl md:text-6xl'>32 hrs</span>
+                            <span className='text-white text-4xl md:text-6xl md:mb-4'>32 hrs</span>
                             <span className='text-white opacity-60'>Last Week-36hrs</span>
                         </div>
                     </div>
@@ -125,7 +126,7 @@ function Dashboard() {
                             <Image className='h-2' src='/images/icon-ellipsis.svg' alt='elipsis image' layout='fixed' width={35} height={10} />
                         </div>
                         <div className='flex  justify-between pt-6 pb-4 md:flex-col md:text-center '>
-                            <span className='text-white text-4xl md:text-6xl'>32 hrs</span>
+                            <span className='text-white text-4xl md:text-6xl md:mb-4'>32 hrs</span>
                             <span className='text-white opacity-60'>Last Week-36hrs</span>
                         </div>
                     </div>
